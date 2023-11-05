9bf939f9ca664cdd6e9a8433fbaa8d2e4b55ec91
Christian Gebbe <>
2023-11-06 00:29:57 +0100

    feat: dummy lambda functino works

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,34 +1,40 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+data "archive_file" "main" {
+  type        = "zip"
+  source_file = "lambda_func.py"
+  output_path = "lambda_func.zip"
 }
 
+
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
 }
 
 
 resource "aws_lambda_function" "main" {
+  filename      = data.archive_file.main.output_path
   function_name = "etl-s3-metadata-to-mongodb"
-  filename      = "lambda_func.py"
-  handler       = "lambda_func.lambda_handler"
-  runtime       = "python3.11"
   role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_func.lambda_handler"
+  runtime       = "python3.9"
 }
 
-
-
 resource "aws_lambda_permission" "main" {
-  statement_id  = "AllowS3Invocation"
+  statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.main.arn
   principal     = "s3.amazonaws.com"
@@ -41,11 +47,9 @@ resource "aws_s3_bucket_notification" "main" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.main.arn
     events              = ["s3:ObjectCreated:*"]
-    # filter_prefix       = "input/"  # Optional: Filter S3 objects by prefix
+    filter_suffix       = "metadata.yaml"
   }
 
-  filter {
-    prefix = ""
-    suffix = "metadata.yaml"
-  }
+  depends_on = [aws_lambda_permission.main]
+
 }
