6a489dcf067e524ed2b8278c14f64d60c0c84750
Christian Gebbe <>
2023-11-06 00:02:23 +0100

    chore: reduce precommit hooks

--- terraform/lambda_func.py
+++ terraform/lambda_func.py
@@ -1,7 +1,5 @@
 import json
 
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Hello from Lambda!")
-    }
+
+def lambda_handler(_event, _context):
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
