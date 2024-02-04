8dbe288326aac31f1fc37e92bf49284aaddc2aad
Christian Gebbe <>
2024-02-04 21:45:30 +0100

    fixup! feat: add tracker

--- src/elliptio/adapters/tracker.py
+++ src/elliptio/adapters/tracker.py
@@ -1,9 +1,10 @@
-import datetime
+from datetime import datetime
 import functools
 import getpass
-import importlib
+import importlib.metadata
 import socket
 import sys
+
 
 from elliptio.interfaces import AutomaticMetadata, TrackerInterface
 
@@ -12,8 +13,8 @@ class Tracker(TrackerInterface):
     def get_automatic_metadata(self) -> AutomaticMetadata:
         return AutomaticMetadata(
             run_id="random_id",
-            creation_time=datetime.datetime.now(tz=datetime.UTC),
-            argv=" ".join(sys.orig_argv),
+            creation_time=datetime.utcnow(),
+            argv=" ".join(sys.argv),  # NOTE: before sys.orig_argv using 3.11 ?!
             username=_get_username(),
             hostname=_get_hostname(),
             python_packages=_get_python_packages(),
@@ -34,7 +35,7 @@ def _get_python_packages():
     """Gets list of installed python packages.
 
     Some alternatives:
-    - `pip freeze`: pip is not installed in rye venvs
+    - `pip freeze`: pip is not installed in rye or pdm venvs
     - `pkg_resources`: deprecated in favor of importlib.resources or importlib.metadata
     """
-    return {d.name: d.version for d in importlib.metadata.distributions()}
+    return {d.metadata["name"]: d.version for d in importlib.metadata.distributions()}
